d5b4be871ecd9dc5e323313635d508f1efdda8b3
heartstchr <heartstchr@live.com>
2016-09-11 21:48:17 +0530

    gulpjs file

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,17 @@ var gulp = require('gulp'),
     util = require('gulp-util'),
     concat = require('gulp-concat'),
     minifycss = require('gulp-minify-css'),
-    uglify = require('gulp-uglify'),
-    imagemin = require('gulp-imagemin');
+    uglify = require('gulp-uglify');
 
 // assets is where you define your application assets and you can pass them into gulp.
-var assets = require('./assets');
 var gulpFileCwd = __dirname +'/server/angular';
 process.chdir(gulpFileCwd);
 util.log('Working directory changed to', util.colors.magenta(gulpFileCwd));
 gulp.task('default', function(){
     gulp.src(gulpFileCwd+'/**/*.js')
         .pipe(concat('main.min.js'))
-        .pipe(uglify())
+        .pipe(uglify().on('error', function(e){
+            console.log(e);
+        }))
         .pipe(gulp.dest(gulpFileCwd+'/'));
 });
